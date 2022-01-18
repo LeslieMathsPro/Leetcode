099acbeb2458dfde989da0c8a5954dd9249a8781
Liwei Xu <liwei.xu@etu.emse.fr>
2022-01-18 01:39:31 +0100

    jump problem

--- Greedy/55.cpp
+++ Greedy/55.cpp
@@ -3,10 +3,10 @@ public:
     bool canJump(vector<int>& nums) {
         int cover = 0;
         if (nums.size() == 1) return true;
-        for (int i = 0; i <= cover; i++){
-            cover = max(i + nums[i], cover);
-            if (cover > nums.size() - 1) return true;
+        for (int i = 0; i < nums.size(); i++){
+            cover = max(cover, i + nums[i]);
+            if (cover >= nums.size() - 1) return true;
         }
         return false;
     }
-};
+};  
